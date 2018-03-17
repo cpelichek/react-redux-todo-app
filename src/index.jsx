24bbb7b0a3abddd6b0617a98e2e627da61150025
caio <cpelichek@gmail.com>
2018-03-16 21:10:59 -0300

    Added devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() to index.js, to integrate with the browser extension installed

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,9 @@ import { Provider } from 'react-redux';
 import App from './main/app.jsx';
 import reducers from './main/reducers';
 
-const store = createStore(reducers);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__    //integra com extensão do navegador (plugin do browser), tem por finalidade ajudar durante o desenvolvimento a enxergar passo-a-passo o que acontece na aplicação, cada ação e mudança de estado
+    && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(reducers, devTools);
 ReactDOM.render(
     <Provider store={store}>
         <App />
