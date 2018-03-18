469d8609fbeff662a16e74856e2a7c2d3f9c8f89
caio <cpelichek@gmail.com>
2018-03-17 21:58:27 -0300

    Added Middleware (Redux-Promise)

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import promise from 'redux-promise';
 
 import App from './main/app.jsx';
 import reducers from './main/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__    //integra com extensão do navegador (plugin do browser), tem por finalidade ajudar durante o desenvolvimento a enxergar passo-a-passo o que acontece na aplicação, cada ação e mudança de estado
     && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(reducers, devTools);
+const store = applyMiddleware(promise)(createStore)(reducers, devTools);        //quando usamos o middleware, ao passarmos uma promise para ele, ele irá esperar a promise carregar os dados para então passar essas informações adiante para o payload
 ReactDOM.render(
     <Provider store={store}>
         <App />
